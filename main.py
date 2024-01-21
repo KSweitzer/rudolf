79ab63abaf29fa161475989701ddf6c5d92f1cc9
KSweitzer <kevint.sweitzer@gmail.com>
2024-01-21 09:47:20 -0800

    Saved reddit post from r/AmItheAsshole into pandas object. Added requirements.txt

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import praw
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 
 def main():
     load_dotenv()
     reddit = praw.Reddit(client_id=os.environ.get("CLIENT_ID"), client_secret=os.environ.get("CLIENT_SECRET"), user_agent=os.environ.get("USER_AGENT"))
-    subreddit = reddit.subreddit("AmItheAsshole").hot(limit=10)
+    subreddit = reddit.subreddit("AmItheAsshole").hot()
 
-    for submission in subreddit:
-        print(submission.title)
+    post_list = []
+
+    for post in subreddit:
+        post_list.append([post.title, post.score, post.url, post.num_comments, post.selftext])
+
+    post_df = pd.DataFrame(post_list, columns=['title', 'score', 'url', 'num_comments', 'body'])
+    print(post_df.head())
+
+    # Now we need to create ai voice with this
     pass
 
 
